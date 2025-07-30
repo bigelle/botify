bddfa5ac910f26508cdca71a2830f65bb730c788
Ruslan Bigeldinov <ruslan.ok04@gmail.com>
2025-07-30 19:39:32 +0500

    feat: merge old gotely games types

--- types.go
+++ types.go
@@ -2358,3 +2358,24 @@ type PassportElementErrorUnspecified struct {
 func (p PassportElementErrorUnspecified) GetPassportElementErrorSource() string {
 	return "unspecified"
 }
+
+/*
+	BEGIN Games TYPES
+*/
+
+type Game struct {
+	Title        string           `json:"title"`
+	Description  string           `json:"description"`
+	Photo        []PhotoSize      `json:"photo"`
+	Text         *string          `json:"text,omitempty"`
+	TextEntities *[]MessageEntity `json:"text_entities,omitempty"`
+	Animation    *Animation       `json:"animation,omitempty"`
+}
+
+type CallbackGame struct{}
+
+type GameHighScore struct {
+	Position int  `json:"position"`
+	User     User `json:"user"`
+	Score    int  `json:"score"`
+}
